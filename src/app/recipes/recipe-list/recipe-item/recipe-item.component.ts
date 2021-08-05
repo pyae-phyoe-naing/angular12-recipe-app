3495c55ee0ed3224409d515fd974887d0e81444a
fazza7777 <aungbarlaay7777@gmail.com>
2021-08-05 13:27:03 +0630

    show recipe detail => passing item-list-recipe-detail step by step

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output,EventEmitter } from '@angular/core';
+
 import { Recipe } from '../../recipe.model';
 
 @Component({
@@ -8,9 +9,12 @@ import { Recipe } from '../../recipe.model';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe !: Recipe;
+  @Output() recipeSelected = new EventEmitter<void>();
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  onSelected(){
+    this.recipeSelected.emit();
+  }
 }
